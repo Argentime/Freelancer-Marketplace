e58c30a1da00d9f02f517241181f812066ece964
Alex <alex_bocharov@tut.by>
2025-02-19 00:39:59 +0300

    Реализован Path GET endpoint, улучшена архитектура лабораторной, добавлен конструктор класса Controller

--- src/main/java/com/example/javalabs/controllers/Controller.java
+++ src/main/java/com/example/javalabs/controllers/Controller.java
@@ -1,32 +1,44 @@
 package com.example.javalabs.controllers;
 
 import com.example.javalabs.models.Freelancer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
-    @GetMapping("/freelancers")
+
+    private List<Freelancer> freelancersList;
+
+    private void createTestFreelancers() {
+        this.freelancersList = new ArrayList<>();
+        freelancersList.add(new Freelancer("Alice", "design", 4.8, 120));
+        freelancersList.add(new Freelancer("Bob", "design", 4.5, 95));
+        freelancersList.add(new Freelancer("Charlie", "development", 4.7, 110));
+        freelancersList.add(new Freelancer("Dave", "development", 4.6, 105));
+    }
+
+    public Controller() {
+        createTestFreelancers();
+    }
+
+    @GetMapping("/freelancers") // http://localhost:8080/api/freelancers?category=cat
     public List<Freelancer> getFreelancers(@RequestParam String category) {
-        switch (category.toLowerCase()) {
-            case "design":
-                return List.of(
-                        new Freelancer("Alice", "Design", 4.8, 120),
-                        new Freelancer("Bob", "Design", 4.5, 95)
-                );
-            case "development":
-                return List.of(
-                        new Freelancer("Charlie", "Development", 4.7, 110),
-                        new Freelancer("Dave", "Development", 4.6, 105)
-                );
-            default:
-                return List.of();
-        }
+        Queue<Freelancer> queue = new LinkedList<>();
+                for(int i=0; i<freelancersList.size(); i++) {
+                    if (freelancersList.get(i).getCategory().equals(category)) {
+                        queue.add(freelancersList.get(i));
+                    }
+                }
+                if (queue.isEmpty()) {
+                    return null;
+                }
+        return queue.stream().toList();
+    }
+
+    @GetMapping("/freelancers/{id}") // http://localhost:8080/api/freelancers/numb
+    public Freelancer getFreelancerById(@PathVariable int id) {
+        return freelancersList.get(id);
     }
 }
