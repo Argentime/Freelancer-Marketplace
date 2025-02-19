670e7c57492a68377c7cbe658e77fadcc67642ed
Alex <alex_bocharov@tut.by>
2025-02-19 08:14:30 +0300

    Реализован Path GET endpoint, улучшена архитектура лабораторной, добавлен конструктор класса Controller

--- src/main/java/com/example/javalabs/controllers/Controller.java
+++ src/main/java/com/example/javalabs/controllers/Controller.java
@@ -8,7 +8,7 @@ import java.util.*;
 @RestController
 @RequestMapping("/api")
 public class Controller {
-
+    //jg
     private List<Freelancer> freelancersList;
 
     private void createTestFreelancers() {
