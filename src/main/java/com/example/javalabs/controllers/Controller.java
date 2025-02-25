9cf21bd43026b00ae8c5c3c303836ca57089e368
Александр Бочаров <alex_bocharov@tut.by>
2025-02-25 12:21:58 +0300

    удалили ненужный импорт

--- src/main/java/com/example/javalabs/controllers/Controller.java
+++ src/main/java/com/example/javalabs/controllers/Controller.java
@@ -2,7 +2,6 @@ package com.example.javalabs.controllers;
 
 import com.example.javalabs.models.Freelancer;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
