09b78f5f3e8ae52c117dbcfaf5efec6b95a01149
Alex <alex_bocharov@tut.by>
2025-04-26 17:01:43 +0300

    контролер переделывается

--- src/main/java/com/example/javalabs/controllers/FreelancersController.java
+++ src/main/java/com/example/javalabs/controllers/FreelancersController.java
@@ -11,27 +11,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @Tag(name = "Freelancers", description = "API for managing freelancers")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FreelancersController {
     private final FreelancerService freelancerService;
-    private final LogService logService;
 
     public FreelancersController(FreelancerService freelancerService, LogService logService) {
         this.freelancerService = freelancerService;
-        this.logService = logService;
     }
 
     @GetMapping("/freelancers")
