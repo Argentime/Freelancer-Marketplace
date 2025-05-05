d8c944b58706193e1462cfcfb6c044d6679ffb95
Alex <alex_bocharov@tut.by>
2025-05-06 01:06:51 +0300

    добавлен url в контроллер и порт в application.properties

--- backend/src/main/java/com/example/javalabs/controllers/FreelancersController.java
+++ backend/src/main/java/com/example/javalabs/controllers/FreelancersController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @Tag(name = "Freelancers", description = "API for managing freelancers")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://freelance-marketplace-frontend.onrender.com"})
 public class FreelancersController {
     private final FreelancerService freelancerService;
 
